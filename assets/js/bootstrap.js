b13076102bf4b480370ede1e1d3daaa6ff7e182e
Jasen <jasenlambright@gmail.com>
2018-11-15 23:59:08 -0500

    Added comment box

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -3,6 +3,9 @@
   * Copyright 2011-2017 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
+
+
+
 var bootstrap = (function (exports,$,Popper) {
 'use strict';
 
@@ -3848,3 +3851,62 @@ return exports;
 
 }({},$,Popper));
 //# sourceMappingURL=bootstrap.js.map
+
+
+/*Donation JS*/
+
+
+var firstName = "";
+    var lastName = "";
+    var email = "";
+    var dType = "";
+    var receipt = "";
+    var anon = "";
+    var list = "";
+    var amount = "";
+    
+    $('.set-amount').autoGrow(0);
+    
+    /*
+      if(isiPad || jQuery.browser.mobile){
+        $('#team').hide()
+        $('.team-mobile').show(); 
+      }else{
+        $('#team').show()
+        $('.team-mobile').hide();
+      }
+    */
+    
+    //Set & Highlight Donation Amount
+    $(".button").click(function(){
+      $(".button").removeClass("selected");
+      $(this).addClass("selected");
+      
+      $(this).find("input").focus();
+    });
+    
+    //Grow the donation box if they type more than 4 numbers
+    $(".set-amount").keyup(function(){
+      
+      if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+           this.value = this.value.replace(/[^0-9\.]/g, '');
+        }
+
+    });
+    
+    
+    $("input").on("change", function(){
+      // $(".donation-box").css("height", "500px");
+      
+      firstName = $("#firstName").val();
+      lastName = $("#lastName").val();
+      email = $("#email").val();
+      
+      if ( $("#one-time").prop( "checked" ) ){
+        dType = "One-Time";
+      }
+      if ( $("#monthly").prop( "checked" ) ){
+        dType = "Monthly";
+      }
+
+    });
